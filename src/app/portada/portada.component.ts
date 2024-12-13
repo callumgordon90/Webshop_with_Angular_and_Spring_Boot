2e4b6f83a5a91ec5a290f60bc5802352c825cfb6
callum <callumjosephiaingordon@gmail.com>
2024-12-13 16:57:42 +0100

    portada folder

---
+++ src/app/portada/portada.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: 'app-portada',
+    templateUrl: './portada.component.html',
+    styleUrls: ['./portada.component.css']
+})
+export class PortadaComponent {
+    titulo = 'Bienvenido a Web-Shop';
+}
