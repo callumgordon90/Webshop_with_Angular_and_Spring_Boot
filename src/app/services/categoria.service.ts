2648bda39fb96ac49dfbc9f542f9e6d1d1257767
callum <callumjosephiaingordon@gmail.com>
2024-12-28 20:12:35 +0100

    added more of the http client to the categoria.service.ts (but it is still not working)

--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -1,7 +1,9 @@
 import {Injectable} from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 import { Categoria } from '../model/categoria'; 
-import { CATEGORIAS } from './mock-categories';
+import { Respuesta } from '../model/respuesta';
+
+//import { CATEGORIAS } from './mock-categories';
 
 @Injectable()
 
@@ -11,11 +13,27 @@ export class CategoriaService {
         return Promise.resolve(CATEGORIAS);
     }
     */
+    
+    private categoriasUrl: string = '/api/categorias';
 
+    constructor(private http: HttpClient) {}
+
+    /*
     getCategorias(): Promise<Categoria[]>{
         return new Promise(resolve => {
             //Simulate the latency of a server with a 2 second delay
                 setTimeout(()=> resolve(CATEGORIAS),2000);
         });
+    }
+        */
+    getCategorias():Promise<Categoria[]>{
+        return this.http.get<Respuesta>(this.categoriasUrl).toPromise().then(
+            response => response.datos as Categoria[]
+        ).catch(this.handleError);
+    }
+
+    private handleError(error: any):Promise<any>{
+        console.error('Ha Ocurrido un error', error);
+        return Promise.reject(error.message || error);
     }
 }
