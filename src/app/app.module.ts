5b493f3682e926ccf42f23ec5fa57278f14e5b50
callum <callumjosephiaingordon@gmail.com>
2024-12-28 19:46:29 +0100

    added httpclient module to allow connection to backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { MenuComponent } from "./menu/menu.component";
 import { FooterComponent } from "./footer/footer.component";
 import { PortadaComponent } from "./portada/portada.component";
+
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { CategoriaService } from "./services/categoria.service";
 
 @NgModule({
     declarations: [
@@ -17,9 +21,12 @@ import { PortadaComponent } from "./portada/portada.component";
     ],
     imports: [
         BrowserModule,
-        FormsModule //<-- importa FormsModule antes de vincuar con [(ngModel)]
+        FormsModule, //<-- importa FormsModule antes de vincuar con [(ngModel)]
+        HttpClient,
+        HttpClientModule
     ],
     providers: [
+                CategoriaService
             ],
     bootstrap: [AppComponent]
 })
