1dc88a5c1c6437250b8fd78fe0cabf7cf28c6689
callum <callumjosephiaingordon@gmail.com>
2025-01-11 22:21:04 +0100

    updated index.js of api to include cestaRoutes

--- server/index.js
+++ server/index.js
@@ -6,9 +6,11 @@ app.use(express.json());
 
 const categoriasRoutes = require('./routes/categoriaRoutes');
 const librosRoutes = require('./routes/libroRoutes');
+const cestaRoutes = require('.routes/cestaRoutes');
 
 app.use('/api/categorias', categoriasRoutes);
 app.use('api/libros', librosRoutes);
+app.use('/api/cesta', cestaRoutes);
 
 const port = 3000;
 app.listen(port);
