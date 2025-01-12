f1837c29003124ce41a202ed559ed41aea3707c7
callum <callumjosephiaingordon@gmail.com>
2025-01-12 20:21:01 +0100

    added pedidosRoutes to index.js

--- server/index.js
+++ server/index.js
@@ -7,10 +7,12 @@ app.use(express.json());
 const categoriasRoutes = require('./routes/categoriaRoutes');
 const librosRoutes = require('./routes/libroRoutes');
 const cestaRoutes = require('.routes/cestaRoutes');
+const pedidosRoutes = require('./routes/pedidoRoutes');
 
 app.use('/api/categorias', categoriasRoutes);
 app.use('api/libros', librosRoutes);
 app.use('/api/cesta', cestaRoutes);
+app.use('/api/pedidos', pedidosRoutes);
 
 const port = 3000;
 app.listen(port);
